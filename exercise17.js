323c750b2ff997238dee85bdf753cbae05d3fa82
smzuluaga <santimzuluaga01@hotmail.com>
2023-06-07 18:36:13 -0500

    Adding exercise16 v1 to my js code history

---
+++ exercise17.js
@@ -0,0 +1,15 @@
+// Diseñe e implemente una función en javascript que reciba un arreglo de números enteros todos positivos y retorna un nuevo arreglo pero sólo con los números que son múltiplos de 10. Además, el arreglo debe estar ordenado de menor a mayor.
+
+let extraerMultiplosDeDiez = (arrNum) => {
+    return arrNum.sort((a,b)=>a-b).filter ((num)=>{
+        return num % 10 === 0
+    })
+
+}
+
+let numbers1 = [20,5,100,81,7,11];
+let numbers2 = [100000,1,120,12];
+
+// Pruebas en consola
+console.log (extraerMultiplosDeDiez(numbers1));
+console.log (extraerMultiplosDeDiez(numbers2));
